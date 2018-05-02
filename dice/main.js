34a0e215ba8874fcf45178f0c232d9b62e7f072f
dice3d <laz@lazs-MBP.lan>
2018-05-02 20:40:43 +1000

    first commit

---
+++ dice/main.js
@@ -0,0 +1,87 @@
+"use strict";
+
+function dice_initialize(container) {
+    $t.remove($t.id('loading_text'));
+    var canvas = $t.id('canvas');
+    canvas.style.width = window.innerWidth - 1 + 'px';
+    canvas.style.height = window.innerHeight - 1 + 'px';
+    var label = $t.id('label');
+    var set = $t.id('set');
+    var selector_div = $t.id('selector_div');
+    var info_div = $t.id('info_div');
+
+    var outcome = 1;
+    var roll_num = 0;
+    on_set_change();
+
+    $t.dice.use_true_random = false;
+
+    function on_set_change(ev) { set.style.width = set.value.length + 3 + 'ex'; }
+    $t.bind(set, 'keyup', on_set_change);
+    $t.bind(set, 'mousedown', function(ev) { ev.stopPropagation(); });
+    $t.bind(set, 'mouseup', function(ev) { ev.stopPropagation(); });
+    $t.bind(set, 'focus', function(ev) { $t.set(container, { class: '' }); });
+    $t.bind(set, 'blur', function(ev) { $t.set(container, { class: 'noselect' }); });
+
+    $t.bind($t.id('clear'), ['mouseup', 'touchend'], function(ev) {
+        ev.stopPropagation();
+        set.value = '0';
+        on_set_change();
+    });
+
+    var box = new $t.dice.dice_box(canvas, { w: 90, h: 160 });
+    box.animate_selector = false;
+
+    $t.bind(window, 'resize', function() {
+        canvas.style.width = window.innerWidth - 1 + 'px';
+        canvas.style.height = window.innerHeight - 1 + 'px';
+        box.reinit(canvas, { w: 90, h: 160 });
+    });
+
+    function show_selector() {
+        info_div.style.display = 'none';
+        selector_div.style.display = 'inline-block';
+        box.draw_selector();
+    }
+
+    function before_roll(vectors, notation, callback) {
+        if ( roll_num > 0) return;        
+        info_div.style.display = 'none';
+        selector_div.style.display = 'none';
+        // do here rpc call or whatever to get your own result of throw.
+        // then callback with array of your result, example:
+        // callback([2, 2, 2, 2]); // for 4d6 where all dice values are 2.
+        callback([outcome]);
+        roll_num = roll_num+1;
+
+    }
+
+    function notation_getter() {
+        return $t.dice.parse_notation(set.value);
+    }
+
+    function after_roll(notation, result) {
+        var res = result.join(' ');
+        if (notation.constant) res += ' +' + notation.constant;
+        if (result.length > 1) res += ' = ' + 
+                (result.reduce(function(s, a) { return s + a; }) + notation.constant);
+        label.innerHTML = res;
+        info_div.style.display = 'inline-block';
+    }
+
+    box.bind_mouse(container, notation_getter, before_roll, after_roll);
+    box.bind_throw($t.id('throw'), notation_getter, before_roll, after_roll);
+    $t.bind(container, ['mouseup'], function(ev) {
+        ev.stopPropagation();
+    });
+
+    set.value = "1d6";
+
+    var params = $t.get_url_params();
+    if(params.sessionid == "p3Zw8Xc5OTrljo") outcome = 6;
+    if(params.sessionid == "p3Zw8Xc5OTrIjo") outcome = 3;
+    if(params.sessionid == "p3Zw8Xo5OTrljo") outcome = 4;
+
+    info_div.style.display = 'inline-block';
+
+}
